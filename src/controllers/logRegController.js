801774fd1cdf1da5f621ab5120fdabbb2bac1c9c
Sorakatop <alcocergonzalomatias@gmail.com>
2021-08-03 19:16:43 -0300

    arreglando controlador de registro

--- src/controllers/logRegController.js
+++ src/controllers/logRegController.js
@@ -3,5 +3,6 @@ const { name, carousel } = productsDB
 
 module.exports = {
     login: (req,res)=>{
-    res.render('loginRegistro'),{title:'Login-Barbanegra'}},
+    res.render('loginRegistro',{title:'Login-Barbanegra'})
+    },
 }
