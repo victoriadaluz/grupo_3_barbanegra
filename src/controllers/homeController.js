6e08e2edd508c73e84bc6c8e7fd19a7d64a02c93
Sorakatop <alcocergonzalomatias@gmail.com>
2021-08-15 03:27:27 -0300

    asd

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+
 const productsFilePath = path.join(__dirname, '../data/productDB.JSON');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
