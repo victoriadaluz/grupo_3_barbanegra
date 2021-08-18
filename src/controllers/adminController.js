b6e81a92bd6ba31d7871ff9dd0bf9f00b0b92d18
Devo Alvarez <devogalvarez@gmail.com>
2021-08-17 21:24:24 -0300

    Agregar Productos

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -2,7 +2,7 @@ const {    Router} = require('express');
 const router = Router();
 const fs = require('fs');
 let {
-    products
+    products,addProduct
 } = require('../data/dataBase');
 
 module.exports = {
@@ -16,7 +16,47 @@ module.exports = {
             products
         })
     },
-    delete: (req, res) => {
+    addProducts: (req,res) =>{
+        res.render('admin/agregarProducto')
+    },
+    uploadNewProduct: (req,res) =>{
+        let lastID = 1
+        products.forEach(product =>{
+            if(product.id > lastID){
+                lastID = product.id
+            }
+        })
+
+        let{name, 
+            brand,
+            price,
+            description,
+            discount,
+            category,
+            subcategory,
+            condition,
+            } = req.body
+
+        let newProduct = {
+            id: lastID + 1,
+            name, 
+            brand,
+            price,
+            description,
+            discount,
+            category,
+            subcategory,
+            condition,
+            imagen : "default-image.png"
+        }
+        
+    
+    products.push(newProduct)
+    addProduct(products)
+    res.redirect('/admin/adminProductos')
+    },
+
+    deleteProduct: (req, res) => {
         products.forEach(product => {
             if(product.id === +req.params.id){
                 let productDestroyer = products.indexOf(products);
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -2,15 +2,22 @@ let express = require('express');
 let router = express.Router();
 let { producto, 
     listarProductos, 
-    formAgregarProducto, 
-    agregarProducto, 
+    addProducts, 
+    uploadNewProduct, 
     index,
     editForm,
-    editarProducto, deleteProduct } = require('../controllers/adminController')
+    editarProducto,
+     deleteProduct } = require('../controllers/adminController')
 
 /* GET Index / Index del admin */
 router.get('/', index)
 router.get ('/productos',listarProductos)
+
+/* GET- FORMULARIO DE AGREGAR PRODUCTO */
+router.get('/agregarProducto', addProducts)
+
+/* POST- FORMULARIO DE PRODUCTO  */
+router.post('/agregarProducto', uploadNewProduct)
 
 /*ELIMINAR producto */
 router.delete('/eliminarProducto:id', deleteProduct)
