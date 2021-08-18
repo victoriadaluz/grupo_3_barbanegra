e0c01099654aa97450353306acc75a828a57fd48
Sorakatop <alcocergonzalomatias@gmail.com>
2021-08-18 17:54:20 -0300

    agregado metodo override y metodo delete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
 const port = 3300;
 /* Routes */
 var indexRouter = require('./routes/indexRouter');
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(methodOverride('_method'))
 
 
 /* Vistas renderizadas */
--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -59,12 +59,13 @@ module.exports = {
     deleteProduct: (req, res) => {
         products.forEach(product => {
             if(product.id === +req.params.id){
-                let productDestroyer = products.indexOf(products);
-                productDestroyer.splice(productDestroyer, 1)
+                let productToDelete = products.indexOf(product);
+                products.splice(productToDelete, 1);
             }
-        })
-        writeJSON(products);
-
-        res.redirect('/productos')
+        })            
+        addProduct(products);
+        res.redirect('/admin')
+        
+       
     }
 }
