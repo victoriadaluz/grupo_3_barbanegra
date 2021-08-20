67a2c368d45b23e44dcdc1270575c69a6360aa4d
Victoria Da Luz <vicky.daluz@gmail.com>
2021-08-20 00:37:20 -0300

    Arreglo productDB1

--- src/data/dataBase.js
+++ src/data/dataBase.js
@@ -1,8 +1,8 @@
 let fs = require('fs');
 module.exports = {
 
-    products: JSON.parse(fs.readFileSync('./src/data/productDB.JSON', 'utf-8')),
+    products: JSON.parse(fs.readFileSync('./src/data/productDB1.JSON', 'utf-8')),
     addProduct: (dataBase) => {
-        fs.writeFileSync('./src/data/productDB.json', JSON.stringify(dataBase), "utf-8")
+        fs.writeFileSync('./src/data/productDB1.json', JSON.stringify(dataBase), "utf-8")
     }
 }
