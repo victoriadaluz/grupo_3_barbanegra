f515aec6b1d6bb58cf455186d5c78a88232d0d0f
Devo Alvarez <devogalvarez@gmail.com>
2021-09-02 21:06:24 -0300

    Nuevo header, modifico base de datos

--- Barbanegra/src/data/dataBase.js
+++ Barbanegra/src/data/dataBase.js
@@ -5,7 +5,7 @@ module.exports = {
     //chic@s pedi a path por que era la unica forma que me ande correctamente y me sobre escriba 
     //dale gon genial
     products: JSON.parse(fs.readFileSync(path.join(__dirname, "/productDB3.json"), "utf-8")),
-    categories: JSON.parse(fs.readFileSync(path.join(__dirname, "/subcategories.json"), "utf-8")),
+    categories: JSON.parse(fs.readFileSync(path.join(__dirname, "/categories.json"), "utf-8")),
     addProduct: (dataBase) => {
         fs.writeFileSync(path.join(__dirname, "../data/productDB3.json"), JSON.stringify(dataBase), "utf-8")
     },
