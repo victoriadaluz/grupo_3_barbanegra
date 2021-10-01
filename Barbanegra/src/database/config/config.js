64983804a88d00eb41db077736f3128542ef7f3e
Devo Alvarez <devogalvarez@gmail.com>
2021-10-01 19:39:21 -0300

    terminando admin

--- Barbanegra/src/database/config/config.js
+++ Barbanegra/src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": '12345678',
+    "password": null,
     "database": "barbanegradb",
     "host": "127.0.0.1",
     "dialect": "mysql"
