3f6ff02b0bb74e45617607f32d51f17c18fd484e
Devo Alvarez <devogalvarez@gmail.com>
2021-10-25 20:02:31 -0300

    Validations edit profile

--- Barbanegra/public/javascript/editProfileValidation.js
+++ Barbanegra/public/javascript/editProfileValidation.js
@@ -1,91 +1,182 @@
-
-let qs = (e) => {
-    return document.querySelector(e)
+function qs(element){
+    return document.querySelector(element)
 }
-window.addEventListener('load', () =>{
-    let $inputEditName = qs('#editName'),
-    $editNameErrors = qs('#errorsNombre'),
-    $inputEditNameContainer = qs('#unombre'),
-    $inputEditLastName = qs ('#editLastName'),
-    $editLastNameErrors = qs('#errorsApellido'),
-    $inputEditLastNameContainer = qs('#papellido'),
-    $formEditUser = qs ('#formEditUser')
-$inputEditName.addEventListener('blur', () => {
-    switch(true){
-        case !$inputEditName.value.trim():
-            $editNameErrors.innerHTML = 'Debes ingresar un nombre' 
-            $editNameErrors.classList.add('text-red-600', 'ml-10', 'font-medium')
-            $inputEditNameContainer.classList.remove('border-green-600')
-            $inputEditNameContainer.classList.add('border-red-600');
-            break;
-        case !regExAlpha.test($inputName.value):
-            $editNameErrors.innerHTML = 'Ingresa un nombre valido'
-            $editNameErrors.classList.add('text-red-600', 'ml-10', 'font-medium');
-            $inputEditNameContainer.classList.remove('border-green-600');
-            $inputEditNameContainer.classList.add('border-red-600');
-            break;
-        case $inputEditName.value.trim().length <= 2:
-            $editNameErrors.innerHTML = 'El nombre debe tener mas  de 2 caracteres'
-            $editNameErrors.classList.remove('ml-16')
-            $editNameErrors.classList.add('text-red-600', 'ml-10', 'font-medium');
-            $inputEditNameContainer.classList.remove('border-green-600');
-            $inputEditNameContainer.classList.add('border-red-600');
-            break;
-        default:
-            $inputEditNameContainer.classList.remove('border-red-600')
-            $inputEditNameContainer.classList.add('border-green-600')
-            $editNameErrors.removeAttribute('class')
-            $editNameErrors.innerHTML = ''
-            break;
-    }
-    
-})
-$inputEditLastName.addEventListener('blur', () => {
-    switch(true){
-        case !$inputEditLastName.value.trim():
-            $editLastNameErrors.innerHTML = 'Debes ingresar un nombre' 
-            $editLastNameErrors.classList.add('text-red-600', 'ml-10', 'font-medium')
-            $inputEditLastNameContainer.classList.remove('border-green-600')
-            $inputEditLastNameContainer.classList.add('border-red-600');
-            break;
-        case !regExAlpha.test($inputName.value):
-            $editLastNameErrors.innerHTML = 'Ingresa un nombre valido'
-            $editLastNameErrors.classList.add('text-red-600', 'ml-10', 'font-medium');
-            $inputEditLastNameContainer.classList.remove('border-green-600');
-            $inputEditLastNameContainer.classList.add('border-red-600');
-            break;
-        case $inputEditLastName.value.trim().length <= 2:
-            $editLastNameErrors.innerHTML = 'El nombre debe tener mas  de 2 caracteres'
-            $editLastNameErrors.classList.remove('ml-16')
-            $editLastNameErrors.classList.add('text-red-600', 'ml-10', 'font-medium');
-            $inputEditLastNameContainer.classList.remove('border-green-600');
-            $inputEditLastNameContainer.classList.add('border-red-600');
-            break;
-        default:
-            $inputEditLastNameContainer.classList.remove('border-red-600')
-            $inputEditLastNameContainer.classList.add('border-green-600')
-            $editLastNameErrors.removeAttribute('class')
-            $editLastNameErrors.innerHTML = ''
-            break;
-    }
-    console.log($editNameErrors)
-})
+window.addEventListener('load', function (){
+    let $inputName = qs('#unombre'),
+    $nameErrors = qs('#errorsNombre'),
+    $inputLastName = qs('#papellido'),
+    $lastNameErrors = qs('#errorsApellido'),
+    $inputEmail = qs('#pemail'),
+    $emailErrors = qs('#errorsEmail'),
+    $inputTel = qs('#telefono'),
+    $telErrors = qs('#errorsTelefono'),
+    $inputAdress = qs('#direccion'),
+    $adressErrors = qs('#errorsDireccion'),
+    $inputNumeration = qs('#numeracion'),
+    $numerationErrors = qs('#errorsNumber'),
+    $inputCity = qs('#ciudad'),
+    $cityErrors= qs('#cityErrors'),
+    $inputProvince = qs('#provincia'),
+    $provinceErrors = qs('#provinceErrors'),
+    $inputPostalCode = qs('#codigoPostal'),
+    $postalCodeErrors = qs('#postalCodeErrors');
+    regExp = /^[A-Za-z\s ]+$/g; 
+    regExp2 = /^[A-Za-z0-9\s ]+$/g; 
+    regExp3 = /^[0-9]*$/;
+    regExp4 = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    regExp5 = /^\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{4}) $ /;
 
-$formEditUser.addEventListener('submit',(e)=>{
-    let error=false;
-    e.preventDefault();        
-    let elementsForm=$formEditUser.elements;
-    for(let i=0; i<elementsForm.length; i++) {
-        if(elementsForm[i].value == ""){
-            elementsForm[i].classList.add('text-red-600', 'ml-10', 'font-medium')
-            $submitLoginErrors.innerHTML = 'Quedaron campos sin completar!'
-            $submitLoginErrors.classList.add('text-red-600', 'ml-10', 'font-medium')
-            error=true;
+    $inputName.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp.test($inputName.value):
+                $nameErrors.innerHTML = 'El nombre es incorrecto'
+                $nameErrors.classList.add('errorV')
+                $inputName.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $nameErrors.classList.remove('errorV')
+                    $inputName.classList.remove('errorVI')
+                    $inputName.classList.add('checkV')
+                    $nameErrors.innerHTML = ''
+                    break;
         }
-    }
-    if(!error){
-        console.log('ok');
-        $formEditUser.submit()
-    };
-})
+    })
+    $inputLastName.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp.test($inputLastName.value):
+                $lastNameErrors.innerHTML = 'El apellido es incorrecto'
+                $lastNameErrors.classList.add('errorV')
+                $inputLastName.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $lastNameErrors.classList.remove('errorV')
+                    $inputLastName.classList.remove('errorVI')
+                    $inputLastName.classList.add('checkV')
+                    $lastNameErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputEmail.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp4.test($inputEmail.value):
+                $emailErrors.innerHTML = 'El email es incorrecto'
+                $emailErrors.classList.add('errorV')
+                $inputEmail.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $emailErrors.classList.remove('errorV')
+                    $inputEmail.classList.remove('errorVI')
+                    $inputEmail.classList.add('checkV')
+                    $emailErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputTel.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp4.test($inputTel.value):
+                $telErrors.innerHTML = 'El número telefónico es incorrecto'
+                $telErrors.classList.add('errorV')
+                $inputTel.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $telErrors.classList.remove('errorV')
+                    $inputTel.classList.remove('errorVI')
+                    $inputTel.classList.add('checkV')
+                    $telErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputAdress.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp.test($inputAdress.value):
+                $adressErrors.innerHTML = 'La dirección es incorrecta'
+                $adressErrors.classList.add('errorV')
+                $inputAdress.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $adressErrors.classList.remove('errorV')
+                    $inputAdress.classList.remove('errorVI')
+                    $inputAdress.classList.add('checkV')
+                    $adressErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputNumeration.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp3.test($inputNumeration.value):
+                $numerationErrors.innerHTML = 'La numeración es incorrecta'
+                $numerationErrors.classList.add('errorV')
+                $inputNumeration.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $numerationErrors.classList.remove('errorV')
+                    $inputNumeration.classList.remove('errorVI')
+                    $inputNumeration.classList.add('checkV')
+                    $numerationErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputCity.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp.test($inputCity.value):
+                $cityErrors.innerHTML = 'La ciudad es incorrecta'
+                $cityErrors.classList.add('errorV')
+                $inputCity.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $cityErrors.classList.remove('errorV')
+                    $inputCity.classList.remove('errorVI')
+                    $inputCity.classList.add('checkV')
+                    $cityErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputProvince.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp.test($inputProvince.value):
+                $provinceErrors.innerHTML = 'La provincia es incorrecta'
+                $provinceErrors.classList.add('errorV')
+                $inputProvince.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $provinceErrors.classList.remove('errorV')
+                    $inputProvince.classList.remove('errorVI')
+                    $inputProvince.classList.add('checkV')
+                    $provinceErrors.innerHTML = ''
+                    break;
+        }
+    })
+    $inputPostalCode.addEventListener('blur', ()=>{
+        switch (true) {
+           case !regExp3.test($inputPostalCode.value):
+                $postalCodeErrors.innerHTML = 'El código postal es incorrecto'
+                $postalCodeErrors.classList.add('errorV')
+                $inputPostalCode.classList.add('errorVI')
+                break;
+             
+        
+                default:
+                    $postalCodeErrors.classList.remove('errorV')
+                    $inputPostalCode.classList.remove('errorVI')
+                    $inputPostalCode.classList.add('checkV')
+                    $postalCodeErrors.innerHTML = ''
+                    break;
+        }
+    })
 })
