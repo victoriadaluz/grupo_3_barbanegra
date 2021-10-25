9c16efb63859275d4ec68a9d523eac2ce02bff56
Devo Alvarez <devogalvarez@gmail.com>
2021-10-25 11:53:05 -0300

    terminating front admin validations

--- Barbanegra/public/javascript/editProductValidation.js
+++ Barbanegra/public/javascript/editProductValidation.js
@@ -12,6 +12,7 @@ window.addEventListener('load', function (){
     $productDescriptionErrors = qs ('#descriptionErrors');
     regExp = /^[A-Za-z\s ]+$/g; 
     regExp2 = /^[A-Za-z0-9\s ]+$/g; 
+    regExp3 = /^[0-9]*$/;
     
 
     $inputProductName.addEventListener('blur', () =>{
@@ -19,14 +20,18 @@ window.addEventListener('load', function (){
             case !$inputProductName.value.trim():
                 $productNameErrors.innerHTML = 'El nombre del producto es obligatorio'
                 $productNameErrors.classList.add('errorV')
+                $inputProductName.classList.add('errorVI')
                 break;
         case !regExp.test($inputProductName.value):
                     $productNameErrors.innerHTML = 'El nombre del producto es incorrecto'
                     $productNameErrors.classList.add('errorV')
+                    $inputProductName.classList.add('errorVI')
                     break;
         
             default:
                 $productNameErrors.classList.remove('errorV')
+                $inputProductName.classList.remove('errorVI')
+                $inputProductName.classList.add('checkV')
                 $productNameErrors.innerHTML = ''
                 break;
         }
@@ -36,10 +41,18 @@ window.addEventListener('load', function (){
             case !$inputProductPrice.value.trim():
                 $productPriceErrors.innerHTML = 'Debe ingresar el precio del producto'
                 $productPriceErrors.classList.add('errorV')
+                $inputProductPrice.classList.add('errorVI')
                 break;
+                case !regExp3.test($inputProductPrice.value):
+                    $productPriceErrors.innerHTML = 'El precio del producto es incorrecto, ingrese sólo números.'
+                    $productPriceErrors.classList.add('errorV')
+                    $inputProductPrice.classList.add('errorVI')
+                    break;
         
             default:
                 $productPriceErrors.classList.remove('errorV')
+                $inputProductPrice.classList.remove('errorVI')
+                $inputProductPrice.classList.add('checkV')
                 $productPriceErrors.innerHTML = ''
                 break;
         }
@@ -49,10 +62,18 @@ window.addEventListener('load', function (){
             case !$inputProductDiscount.value.trim():
                 $productDiscountErrors.innerHTML = 'Si no tiene descuento ponga el número 0'
                 $productDiscountErrors.classList.add('errorV')
+                $inputProductDiscount.classList.add('errorVI')
                 break;
+                case !regExp3.test($inputProductDiscount.value):
+                    $productDiscountErrors.innerHTML = 'El precio del producto es incorrecto, ingrese sólo números.'
+                    $productDiscountErrors.classList.add('errorV')
+                    $inputProductDiscount.classList.add('errorVI')
+                    break;
         
             default:
                 $productDiscountErrors.classList.remove('errorV')
+                $inputProductDiscount.classList.remove('errorVI')
+                $inputProductDiscount.classList.add('checkV')
                 $productDiscountErrors.innerHTML = ''
                 break;
         }
@@ -62,10 +83,13 @@ window.addEventListener('load', function (){
             case !$inputProductDescription.value.trim():
                 $productDescriptionErrors.innerHTML = 'La descripción del producto es obligatoria'
                 $productDescriptionErrors.classList.add('errorV')
+                $inputProductDescription.classList.add('errorVI')
                 break;
         
             default:
                 $productDescriptionErrors.classList.remove('errorV')
+                $inputProductDescription.classList.remove('errorVI')
+                $inputProductDescription.classList.add('checkV')
                 $productDescriptionErrors.innerHTML = ''
                 break;
         }
