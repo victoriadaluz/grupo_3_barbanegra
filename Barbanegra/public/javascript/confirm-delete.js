c6420206764cb7be416f1965098555ecedbb6bba
Sorakatop <alcocergonzalomatias@gmail.com>
2021-11-14 03:50:32 -0300

    swal funcional without api, live search funcional

--- Barbanegra/public/javascript/confirm-delete.js
+++ Barbanegra/public/javascript/confirm-delete.js
@@ -1,38 +1,55 @@
-
-window.addEventListener('load', function(){
-  fetch('http://localhost:3300/api/productos')
-  .then(res => res.json())
-  .then(data=>{
-      console.log(data.data)   
-  
-      let $form = document.querySelector('#form-delete-admin') 
-      let $click = document.querySelector('#deleteUserBtn')
-   
-      $click.addEventListener('click',(e)=>{
-        e.preventDefault();
-          let productoId = data.data.find((producto)=> producto.name);
-          console.log(productoId)      
-                
-          Swal.fire({
-              title: `Estas seguro de eliminar el producto: ${productoId.name}`,
-              text: "Una ves eliminado, se renderizara de vuelta esta vista",
-              icon: 'warning',
-              showCancelButton: true,
-              confirmButtonColor: '#3085d6',
-              cancelButtonColor: '#d33',
-              confirmButtonText: 'Confirmar!'
-            }).then((result) => {
-              if (result.isConfirmed) {
-                Swal.fire(
-                  'Deleted!',
-                  'Your file has been deleted.',
-                  'success',
-                  $form.submit()
-                )
-              }
-            })
+window.addEventListener('load', function () {  
+      //live search on admin
+      let $search = document.querySelector('#input-search')
+      let $result = document.querySelectorAll('#results')
+      $search.addEventListener('keyup', function (e) {
+        console.log(e)
+        if (e.target.value == $search.value) {
+          if (e.key === 'Escape') {
+            e.target.value = ''
+          }
+          $result.forEach(resultado => {
+            if (resultado.textContent.toLocaleLowerCase().includes(e.target.value)) {
+              resultado.classList.remove('liveSearch')
+            } else {
+              resultado.classList.add('liveSearch')
+            }
+          })
+        }
       })
-  })
-    
-})
 
+      
+      let $form = document.querySelectorAll('#form-delete-admin') 
+      let $name = document.querySelector('#name-product')      
+     
+      $form.forEach(resultado=>{
+        resultado.addEventListener('submit',(e)=>{
+          console.log($name)
+          e.preventDefault();  
+            Swal.fire({
+                title: 'Estas seguro de eliminar este producto',
+                text: "Una ves eliminado, se renderizara de vuelta esta vista",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Confirmar!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success',
+                    resultado.submit()
+                  )
+                }
+              })
+        })
+    })
+
+     
+
+
+
+
+})
