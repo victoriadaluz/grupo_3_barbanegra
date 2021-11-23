64e657ba1f2a55f5981c1234d982253ff12632ab
Devo Alvarez <devogalvarez@gmail.com>
2021-11-23 00:56:06 -0300

    ultimos cambios

--- Barbanegra/src/validations/changePw.js
+++ Barbanegra/src/validations/changePw.js
@@ -5,11 +5,11 @@ const db = require('../database/models')
 module.exports = [
     check('passwordNew')
     .notEmpty()
-    .withMessage('El campo de contraseña no puede estar vacio'),
+    .withMessage('El campo de contraseña no puede estar vacío.'),
     
     check('password')
     .notEmpty()
-    .withMessage('El campo de contraseña no puede estar vacio'),
+    .withMessage('El campo de contraseña no puede estar vacío.'),
 
     body('password')
         .custom((value, {req})=> {            
@@ -22,13 +22,13 @@ module.exports = [
             .then((user) => {
                  if(!bcrypt.compareSync(req.body.password, user.password)){                    
                     req.session.user.test=req.session.user.test-1
-                    console.log(`🍔🍔WRONG PASSWORD🍔🍔${req.session.user.test}`);
+                    console.log(`🍔🍔Contraseña incorrecta🍔🍔${req.session.user.test}`);
                     return Promise.reject()
                     
                 }
             }) .catch((err)=>{
-                return Promise.reject('password incorrecta')
+                return Promise.reject('Contraseña incorrecta.')
             })
             
         })
-]    
+]
